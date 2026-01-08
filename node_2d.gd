2ca9478eeb0e7c26f1e7701fbd568bba8903093d
akiru秋留 <aqilfahrialhabsy@gmail.com>
2026-01-08 21:20:17 +0700

    penambahan gerak dan heal

--- node_2d.gd
+++ node_2d.gd
@@ -8,6 +8,8 @@ var giliran = TrunState.player
 # data hp/ stat plaayer sama mob
 var hp_plyer = 100
 var hp_mob = 70
+var max_hp_plyer = 100
+
 @onready var hp_bar_char = $UI/hp_char
 @onready var hp_bar_mob = $UI/hp_mob
 #buat reefern UI
@@ -33,22 +35,24 @@ func start_player_trun() -> void:
 	update_ui_text("serang yuk")
 	serang.visible = true
 	
+	
 # ngirim sinyaal kalo neken tomboll serang
 func _on_serang_pressed() -> void:
 	if giliran != TrunState.player :return
-	
 	serang.visible = false # biar gak ganggu ui (ngumpetin)
 	
 	#algoritma serang 
 	var damage = 10
+	char_node.position = Vector2(mob_node.position.x - 150,mob_node.position.y)
 	hp_mob -=damage
 	hp_bar_mob.value = hp_mob
 	update_ui_text("keruskaan" + str(damage) + "dameng")
 	$mob.modulate = Color.RED
 	await get_tree().create_timer(0.2).timeout
 	$mob.modulate = Color.WHITE
-	
 	await get_tree().create_timer(1.5).timeout # biaar gk nimmpa
+	atur_posisi()
+	
 	
 	# cek musuh mati
 	if hp_mob <= 0:
@@ -65,14 +69,16 @@ func giliran_mob():
 	
 	# Ai mob
 	var damaage = 10
+	mob_node.position = Vector2(char_node.position.x + 150, char_node.position.y)
 	hp_plyer -= damaage
 	hp_bar_char.value = hp_plyer
 	update_ui_text("keruskaan" + str(damaage) + "dameng")
 	$char.modulate = Color.RED
 	await get_tree().create_timer(0.2).timeout
 	$char.modulate = Color.WHITE
-	
 	await get_tree().create_timer(1.5).timeout # biaar gk nimmpa
+	atur_posisi()
+	
 	if hp_plyer <= 0:
 		los_battel()
 	else:
@@ -96,4 +102,23 @@ func atur_posisi():
 	keterngan.position = Vector2(500,50)
 	serang.position = Vector2(200,550)
 
+func heal() -> void:
+	if hp_plyer <= 100:
+		update_ui_text("udah penuh jirr")
+		_on_serang_pressed()
+	serang.visible = false # biar gak ganggu ui (ngumpetin)
 	
+	var hieal = 10 
+	hp_plyer += hieal
+	hp_bar_char.value = hp_plyer
+	update_ui_text("memulihkan " + str(hieal) + " HP")
+	$char.modulate = Color.GREEN
+	await get_tree().create_timer(0.2).timeout
+	$char.modulate = Color.WHITE
+	await get_tree().create_timer(1.5).timeout # biaar gk nimmpa
+	
+	# cek musuh mati
+	if hp_mob <= 0:
+		win_battle()
+	else:
+		giliran_mob()
